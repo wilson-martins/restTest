5d1d96a4f3aa00fef8a92387291081734d62f186
Wilson Martins <wilson@kissolutions.tech>
2021-03-23 20:48:24 -0300

    Add FinancialTransactionsDto class

---
+++ src/main/java/com/bench/resttest/dto/FinancialTransactionsDto.java
@@ -0,0 +1,27 @@
+package com.bench.resttest.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FinancialTransactionsDto {
+
+    @JsonProperty(value = "Date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date date;
+
+    @JsonProperty(value = "Ledger")
+    private String ledger;
+
+    @JsonProperty(value = "Amount")
+    private BigDecimal amount;
+
+    @JsonProperty(value = "Company")
+    private String company;
+}
